5db9f9e90103c3f5241675aaf9c163b65ec40352
Jeremiah <jsnidar@gmail.com>
2021-10-20 18:30:59 -0500

    commit tree 9e815fcd522f8940bf31a9865570970899c5060b

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,36 @@
+require 'pry'
+
 class Triangle
-  # write code here
+
+  def initialize side_1, side_2, side_3
+    @side_1 = side_1
+    @side_2 = side_2
+    @side_3 = side_3
+  end
+
+  class TriangleError < StandardError
+  end
+
+  def side_length_test?
+    @side_1 > 0 and @side_2 > 0 and @side_3 > 0
+  end
+
+  def inequality_test?
+    @side_1 + @side_2 > @side_3 and @side_3 + @side_2 > @side_1 and @side_1 + @side_3 > @side_2
+  end
+
+  def kind 
+    if side_length_test? and inequality_test?
+      if @side_1 == @side_2 and @side_1 == @side_3
+        :equilateral
+      elsif @side_1 == @side_2 or @side_1 == @side_3 or @side_2 == @side_3
+        :isosceles
+      elsif @side_1 != @side_2 and @side_1 != @side_3 and @side_2 != @side_3
+        :scalene
+      end
+    else
+      begin raise TriangleError
+      end
+    end
+  end
 end
